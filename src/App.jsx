6a179bcaaabc8ed7321105bc818b9365acc69c4d
Marc Rodriguez <60396817+MarcAnthony-SVG@users.noreply.github.com>
2020-05-05 10:53:53 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
 import Similar from '../src/Component/Similar_Items_Component.js';
 import AlsoBought from '../src/Component/Customers_Also_Bought.js';
-import '../src/styles/App.scss'
+import Nav from 'react-bootstrap/Nav';
+import '../src/styles/App.scss';
 
 class Main extends React.Component {
   constructor(props) {
@@ -19,23 +20,34 @@ class Main extends React.Component {
 
   tick() {
     this.setState({
-      date: new Date(), 
+      date: new Date(),
     });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="Title">RECOMMENDED FOR YOU</header>
-        <button className="Button1">Similar Items</button>
-        <button className="Button2">Customers Also Bought</button>
-        <Similar/>
-        <AlsoBought /> 
-        <div className="LowerBox">
-        <button id="Button3">Shop All Similar Items</button>
+      <div className='App'>
+        <header className='Title'>RECOMMENDED FOR YOU</header>
+        <Nav fill variant='tabs' defaultActiveKey='/home'>
+          <Nav.Item>
+            <Nav.Link href='/home'>Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey='link-1'>Similar Items</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey='link-2'>Customers Also Bought</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        // <header className='Title'>RECOMMENDED FOR YOU</header>
+        // <button className='Button1'>Similar Items</button>
+        // <button className='Button2'>Customers Also Bought</button>
+        <Similar />
+        <AlsoBought />
+        <div className='LowerBox'>
+          <button id='Button3'>Shop All Similar Items</button>
         </div>
-        </div>
-
+      </div>
     );
   }
 }
